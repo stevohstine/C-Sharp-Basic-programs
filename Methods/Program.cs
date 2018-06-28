4f30c65bb41a8b4a504a2760be4eaad5541fc817
Stine <kaguris96@gmail.com>
2018-06-28 20:03:29 +0300

    update

---
+++ Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class person
+    {
+        private string name;
+        private byte age;
+        private float height;
+        private byte weight;
+    }
+
+    public class post
+    {
+        private person person;
+        private string title;
+        private string description;
+        private DateTime datetime;
+    }
+}
